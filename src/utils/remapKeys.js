0cc2b3500f9c8d33ecc54c726aece05f9066c505
thyeris <thyeris@gmail.com>
2026-09-20 21:52:01 -0300

    fix: rating

--- src/utils/remapKeys.js
+++ src/utils/remapKeys.js
@@ -1,5 +1,15 @@
 export function remapKeys(item) {
-  const rating = item.imdbRating === "N/A" ? 0 : Number(item.imdbRating) || 0;
+  const value = item.imdbRating;
+  const num = Number(value);
+
+  let rating;
+  if (value === "N/A") {
+    rating = "N/A";
+  } else if (value !== null && value !== undefined && value !== "" && !isNaN(num) && num >= 0) {
+    rating = num;
+  } else {
+    rating = null;
+  }
 
   return {
     id: item.imdbID,
